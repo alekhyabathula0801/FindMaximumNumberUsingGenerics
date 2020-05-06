b7ff77d0ccd3d94c1858f113a9aa84ec0554ec8e
arun kumar <alekhya0801@gmail.com>
2020-05-06 21:01:27 +0530

    Refracted code by using generic class to find maximum

--- src/main/java/com/findmaxnumber/Maximum.java
+++ src/main/java/com/findmaxnumber/Maximum.java
@@ -1,6 +1,20 @@
 package com.findmaxnumber;
 
-public class FindMaxNumber {
+public class Maximum<E extends Comparable> {
+
+    E a;
+    E b;
+    E c;
+
+    public Maximum(E a, E b, E c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public E findMax() {
+        return findMax(a,b,c);
+    }
 
     public static <E extends Comparable> E findMax(E a, E b, E c) {
         E maximum = a;
---
+++ src/test/java/com/findmaxnumber/MaximumTest.java
@@ -0,0 +1,69 @@
+package com.findmaxnumber;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaximumTest {
+
+    @Test
+    public void givenMaxIntegerAt1stPosition_ShouldReturnSameNumber() {
+        Maximum<Integer> maximum = new Maximum<>(6,1,2);
+        int max = maximum.findMax();
+        Assert.assertEquals(6,max);
+    }
+
+    @Test
+    public void givenMaxIntegerAt2ndPosition_ShouldReturnSameNumber() {
+        Maximum<Integer> maximum = new Maximum<>(-100,-10,-20);
+        int max = maximum.findMax();
+        Assert.assertEquals(-10,max);
+    }
+
+    @Test
+    public void givenMaxIntegerAt3rdPosition_ShouldReturnSameNumber() {
+        Maximum<Integer> maximum = new Maximum<>(1,-5,10);
+        int max = maximum.findMax();
+        Assert.assertEquals(10,max);
+    }
+
+    @Test
+    public void givenMaxFloatAt1stPosition_ShouldReturnSameNumber() {
+        Maximum<Float> maximum = new Maximum<>(1.3f,1.2f,1.1f);
+        Float max = maximum.findMax();
+        Assert.assertEquals(1.3, max, 0.001);
+    }
+
+    @Test
+    public void givenMaxFloatAt2ndPosition_ShouldReturnSameNumber() {
+        Maximum<Float> maximumNumber = new Maximum<>(-1.6f,-1.58f,-1.59f);
+        Float max = maximumNumber.findMax();
+        Assert.assertEquals(-1.58, max, 0.001);
+    }
+    @Test
+    public void givenMaxFloatAt3rdPosition_ShouldReturnSameNumber() {
+        Maximum<Float> maximum = new Maximum<>(-0.1f,-1.1f,0.0f);
+        Float max = maximum.findMax();
+        Assert.assertEquals(0.0, max, 0.001);
+    }
+
+    @Test
+    public void givenMaxStringAt1stPosition_ShouldReturnSameNumber() {
+        Maximum<String> maximum = new Maximum<>("banana","Banana","Peach");
+        String max = maximum.findMax();
+        Assert.assertEquals("banana", max);
+    }
+
+    @Test
+    public void givenMaxStringAt2ndPosition_ShouldReturnSameNumber() {
+        Maximum<String> maximum = new Maximum<>("Banana","apple","Peach");
+        String max = maximum.findMax();
+        Assert.assertEquals("apple", max);
+    }
+
+    @Test
+    public void givenMaxStringAt3rdPosition_ShouldReturnSameNumber() {
+        Maximum<String> maximum = new Maximum<>("Banana","Apple","Peach");
+        String max = maximum.findMax();
+        Assert.assertEquals("Peach", max);
+    }
+}
