5e5545ef01284a23e4fb4877348377ce8a6e0046
arun kumar <alekhya0801@gmail.com>
2020-05-06 22:35:46 +0530

    Added feature to print maximum values

--- src/main/java/com/maximumgenerics/Maximum.java
+++ src/main/java/com/maximumgenerics/Maximum.java
@@ -12,12 +12,17 @@ public class Maximum<E extends Comparable> {
         return findMax(values);
     }
 
-    public static <E extends Comparable> E findMax(E[] values) {
+    public <E extends Comparable> E findMax(E[] values) {
         E maximum = values[0];
         for ( int i=1; i < values.length; i++) {
             if (values[i].compareTo(maximum) > 0)
                 maximum = values[i];
         }
+        printMax(maximum);
         return maximum;
+    }
+
+    public <E> void printMax(E max) {
+        System.out.println("Maximum = " +max);
     }
 }
